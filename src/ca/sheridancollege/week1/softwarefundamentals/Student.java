de0e042c6864b5419616fcdfdcdbb87ea424298c
nivkapadia <nivkpd@gmail.com>
2024-05-15 16:11:07 -0400

    sakdjfh

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * each student can: say their name (getName)
  * 
  * @author dancye May 7, 2018
- * @ModifiedBy NivKapadia May 15, 2025
+ * @ModifiedBy NivKapadia May 15, 2025asdkjh
  */
 public class Student 
 {
